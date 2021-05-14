c31b582bc73d38b76c848661d2627e11df4c818a
MarkCHYL <2285581945@qq.com>
2021-05-14 22:15:02 +0800

    Site updated: 2021-05-14 22:15:01

--- photos/ins.js
+++ photos/ins.js
@@ -118,7 +118,7 @@
           var liTmpl = "";
           for (var i = 0, len = data.link.length; i < len; i++) {
             var minSrc = 'https://raw.githubusercontent.com/MarkCHYL/BlogPicture/master/min_photos/' + data.link[i];
-            var src = 'https://raw.githubusercontent.com/MarkCHYL/BlogPicture/master/photos' + data.link[i];
+            var src = 'https://raw.githubusercontent.com/MarkCHYL/BlogPicture/master/photos/' + data.link[i];
             var type = data.type[i];
             var target = src + (type === 'video' ? '.mp4' : '.jpg');
             src += '';
