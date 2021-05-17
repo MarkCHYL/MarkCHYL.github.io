9b67df4b1c8d9475d7e9a38ac5cd78c0caa1974a
MarkCHYL <2285581945@qq.com>
2021-05-17 16:48:57 +0800

    Site updated: 2021-05-17 16:48:56

--- photos/ins.js
+++ photos/ins.js
@@ -117,8 +117,8 @@
           var data = res.list[j].arr;
           var liTmpl = "";
           for (var i = 0, len = data.link.length; i < len; i++) {
-            var minSrc = 'https://mark_chen.coding.net/p/blogpicture/d/BlogPicture/git/raw/master/min_photos/' + data.link[i];
-            var src = 'https://mark_chen.coding.net/p/blogpicture/d/BlogPicture/git/raw/master/photos/' + data.link[i];
+            var minSrc = 'https://raw.githubusercontent.com/MarkCHYL/BlogPicture/master/min_photos/' + data.link[i];
+            var src = 'https://raw.githubusercontent.com/MarkCHYL/BlogPicture/master/photos/' + data.link[i];
             var type = data.type[i];
             var target = src + (type === 'video' ? '.mp4' : '.jpg');
             src += '';
